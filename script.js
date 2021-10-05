49c481871ac33b74dce9e3353a0bac0b2e6d1063
Steven Azeka <36303808+sazeka@users.noreply.github.com>
2021-10-05 14:42:42 -0400

    Updated to Vue

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-function myFunction() {
-/* 
+// Schools Component
+Vue.component("schools", {
+  data() {
+    return {
+      output: {}
+    };
+  },
+  mounted() {
+    /* 
 Pulling the processed CSV from Github and parsing the CSV.
 Papa Parse: https://www.papaparse.com/
 */
-$.ajax({
-  url:
-    "https://raw.githubusercontent.com/tsdataclinic/NYC_SchoolData/39a155ccc50a24509c69d35ae4cad3428ff0f44d/data/processed/processed_data.csv?token=AIU7HQEDVKMSBYO4IVHUKMLBLJVRY",
-  success: function (csv) {
-    const output = Papa.parse(csv, {
-      header: true // Convert rows to Objects using headers as properties
+    $.ajax({
+      url:
+        "https://raw.githubusercontent.com/sazeka/sazeka.github.io/main/processed_data.csv",
+      success: csv => {
+        this.output = Papa.parse(csv, {
+          header: true // Convert rows to Objects using headers as properties
+        });
+
+        if (this.output.data) {
+          console.log("Loaded");
+        } else {
+          console.log(this.output.errors);
+        }
+      },
+      error: function (jqXHR, textStatus, errorThrow) {
+        console.log(textStatus);
+      }
     });
-    if (output.data) {
-      console.log(output.data);
-    } else {
-      console.log(output.errors);
+  },
+  methods: {
+    test() {
+      console.log(this.output.data[1].DBN);
     }
   },
-  error: function (jqXHR, textStatus, errorThrow) {
-    console.log(textStatus);
+
+  // Template to show on screen
+  template: `<div>
+      <v-btn @click="test">Test</v-btn>
+    </div>`
+});
+
+// Core App
+new Vue({
+  el: "#app",
+  vuetify: new Vuetify(),
+  data() {
+    return {
+    };
   }
 });
-}
